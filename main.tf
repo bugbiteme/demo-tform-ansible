666474f52d15a7c5f8cde07294d1f23f9bc52359
bugbiteme <leon.s.levy@gmail.com>
2020-10-06 14:09:29 -0700

    first commit

---
+++ main.tf
@@ -0,0 +1,115 @@
+provider "aws" {
+  region  = "us-west-1"
+}
+
+// Generate the SSH keypair that we’ll use to configure the EC2 instance. 
+// After that, write the private key to a local file and upload the public key to AWS
+
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+}
+ 
+resource "local_file" "private_key" {
+  filename          = "${path.module}/ansible-key.pem"
+  sensitive_content = tls_private_key.key.private_key_pem
+  file_permission   = "0400"
+}
+ 
+resource "aws_key_pair" "key_pair" {
+  key_name   = "ansible-key"
+  public_key = tls_private_key.key.public_key_openssh
+}
+
+
+// Create a security group with access to port 22 and port 80 open to serve HTTP traffic
+
+data "aws_vpc" "default" {
+  default = true
+}
+ 
+resource "aws_security_group" "allow_ssh" {
+  vpc_id = data.aws_vpc.default.id
+ 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+// Get latest Ubuntu AMI, so that we can configure the EC2 instance
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+ 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+ 
+  owners = ["099720109477"]
+}
+
+// Configure the EC2 instance itself
+
+resource "aws_instance" "ansible_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  key_name               = aws_key_pair.key_pair.key_name
+ 
+  tags = {
+    Name = "Ansible Server"
+  }
+ 
+ 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y"
+      //"software-properties-common -y",
+      //"sudo apt-add-repository --yes --update ppa:ansible/ansible",
+      //"sudo apt install -y ansible"
+    ]
+ 
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      host        = self.public_ip
+      private_key = tls_private_key.key.private_key_pem
+    }
+  }
+ 
+  provisioner "local-exec" {
+    command = "sleep 10"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -u ubuntu --key-file ansible-key.pem -T 300 -i '${self.public_ip},', app.yml"
+  }
+ 
+}
+
+// Output the public_ip and the Ansible command for running the playbook
+
+output "public_ip" {
+ value = aws_instance.ansible_server.public_ip
+}
+ 
+output "ansible_command" {
+    value = "ansible-playbook -u ubuntu --key-file ansible-key.pem -T 300 -i '${aws_instance.ansible_server.public_ip},', app.yml"
+}
