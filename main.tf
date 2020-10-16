f8536c8f0b7fc86615b1237212cc7f2de3958c3c
bugbiteme <leon.s.levy@gmail.com>
2020-10-16 12:01:11 -0700

    simplified code

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region  = "us-west-1"
+  region = "us-west-1"
 }
 
 // Generate the SSH keypair that we’ll use to configure the EC2 instance. 
@@ -8,13 +8,13 @@ provider "aws" {
 resource "tls_private_key" "key" {
   algorithm = "RSA"
 }
- 
+
 resource "local_file" "private_key" {
   filename          = "${path.module}/ansible-key.pem"
   sensitive_content = tls_private_key.key.private_key_pem
   file_permission   = "0400"
 }
- 
+
 resource "aws_key_pair" "key_pair" {
   key_name   = "ansible-key"
   public_key = tls_private_key.key.public_key_openssh
@@ -26,24 +26,24 @@ resource "aws_key_pair" "key_pair" {
 data "aws_vpc" "default" {
   default = true
 }
- 
+
 resource "aws_security_group" "allow_ssh" {
   vpc_id = data.aws_vpc.default.id
- 
+
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- 
+
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- 
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -56,12 +56,12 @@ resource "aws_security_group" "allow_ssh" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
- 
+
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
- 
+
   owners = ["099720109477"]
 }
 
@@ -72,20 +72,17 @@ resource "aws_instance" "ansible_server" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   key_name               = aws_key_pair.key_pair.key_name
- 
+
   tags = {
     Name = "Ansible Server"
   }
- 
- 
+
+
   provisioner "remote-exec" {
     inline = [
       "echo \"Hello World!\""
-      //"software-properties-common -y",
-      //"sudo apt-add-repository --yes --update ppa:ansible/ansible",
-      //"sudo apt install -y ansible"
     ]
- 
+
     connection {
       type        = "ssh"
       user        = "ubuntu"
@@ -94,7 +91,7 @@ resource "aws_instance" "ansible_server" {
     }
   }
 
- /*provisioner "local-exec" {
+  /*provisioner "local-exec" {
     command = "sleep 10"
   }
 
@@ -102,7 +99,7 @@ resource "aws_instance" "ansible_server" {
     command = "ansible-playbook -u ubuntu --key-file ansible-key.pem -T 300 -i '${self.public_ip},', app.yml"
   }
   */
- 
+
 }
 
 // Output the public_ip and the Ansible command for running the playbook
@@ -121,7 +118,7 @@ output "connection_string" {
   description = "Copy/Paste/Enter - You are in the matrix"
   value       = "ssh -i ansible-key.pem ubuntu@${aws_instance.ansible_server.public_dns}"
 }
- 
+
 output "ansible_command" {
-    value = "ansible-playbook -u ubuntu --key-file ansible-key.pem -T 300 -i '${aws_instance.ansible_server.public_ip},', app.yml"
+  value = "ansible-playbook -u ubuntu --key-file ansible-key.pem -T 300 -i '${aws_instance.ansible_server.public_ip},', app.yml"
 }
